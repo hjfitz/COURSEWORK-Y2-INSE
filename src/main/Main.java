11df808055280dd05bb3f35dc02e98ce814fcf12
harry <harry@thebe.local>
2017-04-24 18:32:27 +0100

    begin work on documentation

--- src/main/Main.java
+++ src/main/Main.java
@@ -55,18 +55,16 @@ public class Main extends JFrame {
 	private String currentTime;
 	private String estimateVal;
 	private SimpleDateFormat format;
-	
 
 	/**
-	 * Launch the application.
+	 * Launch the application. Run with the class
+	 * Attempts to a new Main object called 'frame' and calls the method .setVisible
 	 */
-
-	public static void main(String[] args) {
+	public static void main(String args[]) {
 		EventQueue.invokeLater(new Runnable() {
 			@Override
 			public void run() {
 				try {
-					
 					Main frame = new Main();
 					frame.setVisible(true);
 				} catch (Exception e) {
@@ -125,40 +123,21 @@ public class Main extends JFrame {
 		comboBox.setFont(new Font("Lucida Grande", Font.PLAIN, 16));
 		comboBox.setForeground(new Color(0, 0, 128));
 		comboBox.setToolTipText("");
-		comboBox.setModel(new DefaultComboBoxModel(
-				new String[] {
-					"Locks Way Road",
-					"Lidl",
-					"Fratton Station",
-					"Cambridge Road",
-					"Winston Churchill Ave"
-				})
-			);
+		comboBox.setModel(new DefaultComboBoxModel(new String[] { "Locks Way Road", "Lidl", "Fratton Station",
+				"Cambridge Road", "Winston Churchill Ave" }));
 
 		comboBox.setBounds(251, 71, 282, 47);
-		
+
 		panel.add(comboBox);
 
 		JComboBox combo_Arrive = new JComboBox();
 		combo_Arrive.setFont(new Font("Lucida Grande", Font.PLAIN, 16));
 		combo_Arrive.setForeground(new Color(0, 0, 128));
-		combo_Arrive.setModel(new DefaultComboBoxModel(
-				new String[] { 
-					"Locks Way Road",
-					"Lidl",
-					"Fratton Station",
-					"Cambridge Road",
-					"Winston Churchill Ave"
-					})
-				);
+		combo_Arrive.setModel(new DefaultComboBoxModel(new String[] { "Locks Way Road", "Lidl", "Fratton Station",
+				"Cambridge Road", "Winston Churchill Ave" }));
 		combo_Arrive.setSelectedIndex(2);
 		combo_Arrive.setBounds(251, 130, 282, 37);
 		panel.add(combo_Arrive);
-		
-
-		
-		
-
 
 		JLabel lblTo = new JLabel("Depart from");
 		lblTo.setForeground(new Color(0, 0, 128));
@@ -200,16 +179,17 @@ public class Main extends JFrame {
 				txtHints.setText("Enter the time you want to arrive / depart at");
 			}
 		});
-		Date date = new Date();   // given date
-		Calendar calendar = GregorianCalendar.getInstance(); // creates a new calendar instance
-		calendar.setTime(date);   // assigns calendar to given date 
-		 // gets hour in 24h format
+		Date date = new Date(); // given date
+		Calendar calendar = GregorianCalendar.getInstance(); // creates a new
+																// calendar
+																// instance
+		calendar.setTime(date); // assigns calendar to given date
+		// gets hour in 24h format
 		DateFormat formatter = new SimpleDateFormat("hh:mm");
 		txtHour.setText(Integer.toString(calendar.get(Calendar.HOUR_OF_DAY)));
 		txtHour.setBounds(278, 200, 71, 53);
 		panel.add(txtHour);
 		txtHour.setColumns(2);
-		
 
 		JLabel lblArriveAt = new JLabel("Arrive at\n");
 		lblArriveAt.setForeground(new Color(0, 0, 128));
@@ -235,17 +215,17 @@ public class Main extends JFrame {
 				conn.connect();
 				String from = comboBox.getSelectedItem().toString();
 				String to = combo_Arrive.getSelectedItem().toString();
-				if(departing){
+				if (departing) {
 					fixedHour = Integer.parseInt(txtHour.getText()) - 1;
 					fixedMin = Integer.parseInt(txtMinute.getText());
-				}else{
+				} else {
 					fixedHour = Integer.parseInt(txtHour.getText());
-					fixedMin = Integer.parseInt(txtMinute.getText()) + 15 ;
+					fixedMin = Integer.parseInt(txtMinute.getText()) + 15;
 				}
 				String hour = fixedHour + ":" + fixedMin;
 				@SuppressWarnings("unused")
 				String route = "";
-				
+
 				search(from, to, hour, departing, table_2);
 
 			}
@@ -268,7 +248,7 @@ public class Main extends JFrame {
 
 			}
 		});
-		
+
 		panel.add(btn_font);
 		lbl_estimate = new JLabel("Estimated Time until next bus: ");
 		lbl_estimate.setHorizontalAlignment(SwingConstants.CENTER);
@@ -276,7 +256,7 @@ public class Main extends JFrame {
 		lbl_estimate.setForeground(new Color(0, 0, 128));
 		lbl_estimate.setBounds(10, 432, 745, 25);
 		panel.add(lbl_estimate);
-		
+
 		panel_1 = new JPanel();
 		panel_1.setBounds(10, 314, 765, 113);
 		panel.add(panel_1);
@@ -288,11 +268,9 @@ public class Main extends JFrame {
 
 		table_2 = new JTable();
 		table_2.setFont(new Font("Lucida Grande", Font.PLAIN, 15));
-		
-		
-	
+
 		scrollPane.setViewportView(table_2);
-		
+
 		JLabel lblNewLabel = new JLabel("New label");
 		scrollPane.setColumnHeaderView(lblNewLabel);
 
@@ -313,62 +291,55 @@ public class Main extends JFrame {
 		panel.add(txtpnPopRoutes);
 
 		getOrderedPopularRoutes(txtpnPopRoutes);
-		
+
 		txtMinute = new JTextField();
 		txtMinute.setName("minute");
 		txtMinute.setHorizontalAlignment(SwingConstants.CENTER);
 		txtMinute.setFont(new Font("Lucida Grande", Font.PLAIN, 20));
 		String minute = Integer.toString(calendar.get(Calendar.MINUTE));
-		if(Integer.parseInt(minute) < 10){
+		if (Integer.parseInt(minute) < 10) {
 			minute = "0" + minute;
 		}
 		txtMinute.setText(minute);
 		txtMinute.setColumns(2);
 		txtMinute.setBounds(423, 200, 71, 53);
 		panel.add(txtMinute);
-		
+
 		currentTime += txtHour.getText() + ":" + txtMinute.getText() + ":" + "00";
-		
+
 		JLabel label = new JLabel(":");
 		label.setFont(new Font("Tahoma", Font.PLAIN, 25));
 		label.setBounds(382, 209, 12, 37);
 		panel.add(label);
-		
+
 		lblSorryNoRoutes = new JLabel("Sorry, no routes have been found");
 		lblSorryNoRoutes.setHorizontalAlignment(SwingConstants.CENTER);
 		lblSorryNoRoutes.setFont(new Font("Lucida Grande", Font.PLAIN, 25));
 		lblSorryNoRoutes.setBounds(10, 349, 765, 37);
 		panel.add(lblSorryNoRoutes);
-		
-		
-		
-		
-		comboBox.addActionListener(new ActionListener(){
 
-			
+		comboBox.addActionListener(new ActionListener() {
+
 			public void actionPerformed(ActionEvent e) {
 				disableSearch(comboBox, combo_Arrive, btn_Search);
 			}
-			
-		});
-		
-		combo_Arrive.addActionListener(new ActionListener(){
 
-			
+		});
+
+		combo_Arrive.addActionListener(new ActionListener() {
+
 			public void actionPerformed(ActionEvent e) {
 				disableSearch(comboBox, combo_Arrive, btn_Search);
-			}	
-			
+			}
+
 		});
-		
-		
-		txtHour.getDocument().addDocumentListener(new DocumentListener(){
+
+		txtHour.getDocument().addDocumentListener(new DocumentListener() {
 
 			@Override
 			public void insertUpdate(DocumentEvent e) {
-					validInput(txtHour, btn_Search);
+				validInput(txtHour, btn_Search);
 			}
-
 
 			@Override
 			public void removeUpdate(DocumentEvent e) {
@@ -378,16 +349,15 @@ public class Main extends JFrame {
 			@Override
 			public void changedUpdate(DocumentEvent e) {
 			}
-			
+
 		});
-		
-		txtMinute.getDocument().addDocumentListener(new DocumentListener(){
+
+		txtMinute.getDocument().addDocumentListener(new DocumentListener() {
 
 			@Override
 			public void insertUpdate(DocumentEvent e) {
-					validInput(txtMinute, btn_Search);
+				validInput(txtMinute, btn_Search);
 			}
-
 
 			@Override
 			public void removeUpdate(DocumentEvent e) {
@@ -397,97 +367,111 @@ public class Main extends JFrame {
 			@Override
 			public void changedUpdate(DocumentEvent e) {
 			}
-			
+
 		});
 
 	}
+
 	
+	/**
+	 * Searches for a bug
+	 * @param from
+	 * @param to
+	 * @param hour
+	 * @param departing
+	 * @param ourTable
+	 */
 	public void search(String from, String to, String hour, Boolean departing, JTable ourTable) {
+		
+		//create a new database connection and connect
 		DatabaseConnection conn = new DatabaseConnection();
 		conn.connect();
+		
+		// create lists to store the returned busses from the select queries.
+		// these are used later on to populate a new DefaultTableModel which 'ourTable' will be set to
 		ArrayList<BusStop> fromStop = new ArrayList<BusStop>();
 		ArrayList<BusStop> toStop = new ArrayList<BusStop>();
+		
+		// perform a search for a specific route, and popular router given the parameters
 		ResultSet rs1 = conn.getSpecificRoute(from, hour, departing);
 		ResultSet popRoutes = conn.getPopRoutes(from, hour, departing);
-		System.out.println("Stop IDs");
+
+		// try to loop through popular routes and update the hits
+		// this chunk of code uses the same routes that our search query may return.
+		// because these route IDs are returned, and possibly returned often - they may be of use to some users
+		// therefore, there is a table for them, with a 'count' of how often they are searched.
+		// this try-catch chunk of code updates the routes (increasing their hit count) returned in the search
 		try {
-			// only runs if there's data returned, therefore no
-			// validation is requried.
 			while (popRoutes.next()) {
+				// get attributes for a future insert query
 				int curID = popRoutes.getInt("Route_ID");
 				int hitCount = popRoutes.getInt("Route_Count");
+				
+				//increase the hit count
 				int newHits = hitCount + 1;
+				
+				//create a new query and run it. No need for sanitization because there should be no user input.
 				String query = "update Popular set Route_Count=" + newHits + " where Route_ID=" + curID;
 				conn.runInsert(query);
 			}
 		} catch (SQLException e3) {
 			e3.printStackTrace();
 		}
-		System.out.println("end of stop ids");
+		
+		//Create rs2 outside of the try-while-if scope, for use later on
 		ResultSet rs2;
-		String time;
-		try {
-			while (rs1.next()) {
-				time = rs1.getTime("Arrival_Time").toString();
+		
+		try 
+		{
+			//loop over the result set and populate fromStop with new BusStop items
+			while (rs1.next()) 
+			{
+				//get the attributes from the query
+				String time     = rs1.getTime("Arrival_Time").toString();
 				String stopName = rs1.getString("Stop_Name");
+				
+				//make a new bus stop with the attributes gotten two lines ago
 				fromStop.add(new BusStop(stopName, time));
 			}
+			
+			// if we want to see which times are departing,
+			// and the stop we're leaving from isn't empty:
 			if (departing && !fromStop.isEmpty()) {
-				System.out.println("sds");
 				int i = 0;
 				rs2 = conn.getSpecificRoute(to, fromStop.get(0).getTime(), departing);
-				
-				
+
 				Date currentT = format.parse(currentTime);
 				Date arriveTime = format.parse(fromStop.get(i).getTime());
-				String diff = timeDifference(currentT,arriveTime);
+				String diff = timeDifference(currentT, arriveTime);
 				lbl_estimate.setText(diff);
-				
-				
+
 			} else {
 				rs2 = conn.getSpecificRoute(to, hour, departing);
-				
+
 			}
-			
-			if(fromStop.isEmpty()){
+
+			if (fromStop.isEmpty()) {
 				panel_1.setVisible(false);
 				lblSorryNoRoutes.setVisible(true);
-			}
-			else{
+			} else {
 				panel_1.setVisible(true);
 				lblSorryNoRoutes.setVisible(false);
 			}
 			while (rs2.next()) {
-				time = rs2.getTime("Arrival_Time").toString();
+				String time = rs2.getTime("Arrival_Time").toString();
 				String stopName = rs2.getString("Stop_Name");
 				toStop.add(new BusStop(stopName, time));
 			}
 			DefaultTableModel model = new DefaultTableModel(
-					new String[] { 
-							"Depart at",
-							"Arrive at",
-							"From",
-							"To",
-							"Travel time",
-							"Estimated Price"
-							},
-					0);
+					new String[] { "Depart at", "Arrive at", "From", "To", "Travel time", "Estimated Price" }, 0);
 			for (int i = 0; i < toStop.size(); i++) {
 				String travel = fromStop.get(i).calculateTravelTime(toStop.get(i).getTime());
 				String price = fromStop.get(i).calculateCost(toStop.get(i).getTime());
 				String[] timeFrom = fromStop.get(i).getTime().split(":");
 				String[] timeTo = toStop.get(i).getTime().split(":");
 				if (!travel.equals("past")) {
-					model.addRow(
-						new Object[] {
-							timeFrom[0] + ":" + timeFrom[1],
-							timeTo[0] + ":" + timeTo[1],
-							fromStop.get(i).getBusName(),
-							toStop.get(i).getBusName(),
-							travel,
-							price
-							}
-						);
+					model.addRow(new Object[] { timeFrom[0] + ":" + timeFrom[1], timeTo[0] + ":" + timeTo[1],
+							fromStop.get(i).getBusName(), toStop.get(i).getBusName(), travel, price });
 					ourTable.setModel(model);
 				}
 			}
@@ -500,14 +484,13 @@ public class Main extends JFrame {
 			e1.printStackTrace();
 		}
 		conn.closeConnection();
-		
-		
+
 		ListSelectionModel cellSelectionModel = table_2.getSelectionModel();
 		cellSelectionModel.addListSelectionListener(new ListSelectionListener() {
 
 			@Override
 			public void valueChanged(ListSelectionEvent e) {
-			
+
 			}
 		});
 	}
@@ -531,82 +514,76 @@ public class Main extends JFrame {
 
 	public void changeFontSize(Boolean enlarge, JComponent[] elementList) {
 		if (enlarge) {
-		  for (JComponent element : elementList) {
-			element.setFont(new Font("Arial", Font.PLAIN, element.getFont().getSize() + 5));
-			btn_font.setText("Decrease Font Size");
-		  }
+			for (JComponent element : elementList) {
+				element.setFont(new Font("Arial", Font.PLAIN, element.getFont().getSize() + 5));
+				btn_font.setText("Decrease Font Size");
+			}
 		} else {
-		  for (JComponent element : elementList) {
-			element.setFont(new Font("Arial", Font.PLAIN, element.getFont().getSize() - 5));
-			btn_font.setText("Increase Font Size");
-		  }
+			for (JComponent element : elementList) {
+				element.setFont(new Font("Arial", Font.PLAIN, element.getFont().getSize() - 5));
+				btn_font.setText("Increase Font Size");
+			}
 		}
 	}
-	
-	public void disableSearch(JComboBox<String> comboBox, JComboBox<String> combo_Arrive, JButton btn_Search){
+
+	public void disableSearch(JComboBox<String> comboBox, JComboBox<String> combo_Arrive, JButton btn_Search) {
 		String from = comboBox.getSelectedItem().toString();
 		String to = combo_Arrive.getSelectedItem().toString();
-		if(from.equals(to)){
+		if (from.equals(to)) {
 			btn_Search.setEnabled(false);
-		}else{
+		} else {
 			btn_Search.setEnabled(true);
 		}
 	}
-	
-	
-	public void validInput(JTextField txt,JButton btn_Search){
+
+	public void validInput(JTextField txt, JButton btn_Search) {
 		String text = txt.getText();
-		if(text.isEmpty()){
+		if (text.isEmpty()) {
 			btn_Search.setEnabled(false);
-		}
-		else {
-	
+		} else {
+
 			try {
 				int val = Integer.parseInt(text);
-				if(txt.getName().equals("hour")){
-					if(val > 24){
+				if (txt.getName().equals("hour")) {
+					if (val > 24) {
 						btn_Search.setEnabled(false);
-					}else{
+					} else {
+						btn_Search.setEnabled(true);
+					}
+				} else if (txt.getName().equals("minute")) {
+					if (val > 59) {
+						btn_Search.setEnabled(false);
+					} else {
 						btn_Search.setEnabled(true);
 					}
 				}
-				else if(txt.getName().equals("minute")){
-					if(val > 59){
-						btn_Search.setEnabled(false);
-					}
-					else{
-						btn_Search.setEnabled(true);
-					}
-				}
-	
+
 			} catch (NumberFormatException e) {
 				btn_Search.setEnabled(false);
 			}
 		}
 	}
-	
-	
-	
-	public String timeDifference(Date time1, Date time2) throws ParseException{
-		
+
+	public String timeDifference(Date time1, Date time2) throws ParseException {
+
 		long timeDiff = time2.getTime() - time1.getTime();
 		long estimateH = (timeDiff / 3600000);
-        long estimateM = (timeDiff % 3600000) / 60000;
-        System.out.println(estimateH);
-        System.out.println(estimateM);
-        
-        if(estimateM > 0){
-        	if(estimateH == 0){
+		long estimateM = (timeDiff % 3600000) / 60000;
+		System.out.println(estimateH);
+		System.out.println(estimateM);
+
+		if (estimateM > 0) {
+			if (estimateH == 0) {
 				estimateVal = ("Estimated Time until next bus: " + estimateM + " minutes");
-			}else{
+			} else {
 				estimateVal = ("Estimated Time until next bus: " + estimateH + " hours and " + estimateM + " minutes");
 			}
-        }
-        
-        if(estimateVal.length() > 0){
-        	lbl_estimate.setVisible(true);
-        }
-        
-        return estimateVal;
+		}
+
+		if (estimateVal.length() > 0) {
+			lbl_estimate.setVisible(true);
+		}
+
+		return estimateVal;
 	}
 }
